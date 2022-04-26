6706a8cb67b701e595c477fc1d58a19d03b2b8d7
Mateus Silverio <mateussilverio@Mac-mini-de-Mateus.local>
2022-04-25 22:05:25 -0300

    primeiro commit

---
+++ IMC/IMC/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Mateus Silverio on 03/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // Teste Commit inicial
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var ivResult: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func calculate(_ sender: Any) {
+    }
+    
+}
+
